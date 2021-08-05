8f3ad17f36f4116985f91bae3753cc6310d08ae0
FizzAleem <fizza.aleem@devsinc.com>
2021-08-04 23:43:04 -0400

    Product images

--- app/helpers/products_helper.rb
+++ app/helpers/products_helper.rb
@@ -1,12 +1,15 @@
 module ProductsHelper
-  
+
 
   def product_thumbnail product
 
-    product.images.each do |image|
+    tag("img",src:"#{url_for(product.images[0])}", width:"100%",height:"auto")
 
-      image_tag url_for(image)
 
-    end
+    # product.images.each do |image|
+      # "<img src='#{product.images[0]}>".html_safe
+      # image_tag url_for(image)
+
+    # end
   end
 end
